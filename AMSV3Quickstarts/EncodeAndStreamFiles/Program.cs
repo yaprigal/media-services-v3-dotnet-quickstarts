7aadf920ac449efefcc01f51c4741ab49ff2f421
John Deutscher <johndeu@microsoft.com>
2018-04-30 13:15:06 -0700

    build2018

---
+++ AMSV3Quickstarts/EncodeAndStreamFiles/Program.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Management.Media;
+using Microsoft.Azure.Management.Media.Models;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Newtonsoft.Json;
+
+namespace EncodeAndStreamFiles
+{
+    class Program
+    {
+        private const string AdaptiveStreamingTransformName = "MyTransformWithAdaptiveStreamingPreset";
+        private const string PredefinedClearStreamingOnly = "Predefined_ClearStreamingOnly";
+        private const string OutputFolder = @"Output";
+
+        static void Main(string[] args)
+        {
+            ConfigWrapper config = new ConfigWrapper();
+            try
+            {
+                IAzureMediaServicesClient client = CreateMediaServicesClient(config);
+
+<<<<<<< HEAD
+            IAzureMediaServicesClient client = CreateMediaServicesClient(config);
+
+
+            // Creating a unique suffix so that we don't have name collisions if you run the sample
+            // multiple times without cleaning up.
+            string uniqueness = Guid.NewGuid().ToString().Substring(0, 13);
+
+            string jobName = "job-" + uniqueness;
+            string locatorName = "locator-" + uniqueness;
+            string outputAssetName = "output-" + uniqueness;
+=======
+                string jobName = "job-" + Guid.NewGuid().ToString();
+                string locatorName = "locator-" + Guid.NewGuid().ToString();
+                string outputAssetName = "output" + Guid.NewGuid().ToString();
+>>>>>>> f60306445f13451672f80a514b740f6ccffb1ac7
+
+                Transform transform = EnsureTransformExists(client, config.ResourceGroup, config.AccountName, AdaptiveStreamingTransformName);
+
+                Asset outputAsset = client.Assets.CreateOrUpdate(config.ResourceGroup, config.AccountName, outputAssetName, new Asset());
+
+                Job job = SubmitJob(client, config.ResourceGroup, config.AccountName, AdaptiveStreamingTransformName, outputAsset.Name, jobName);
+
+                job = WaitForJobToFinish(client, config.ResourceGroup, config.AccountName, AdaptiveStreamingTransformName, jobName);
+
+                if (job.State == JobState.Finished)
+                {
+                    Console.WriteLine("Job finished.");
+                    if (!Directory.Exists(OutputFolder))
+                        Directory.CreateDirectory(OutputFolder);
+
+                    DownloadResults(client, config.ResourceGroup, config.AccountName, outputAsset.Name, OutputFolder);
+
+                    StreamingLocator locator = CreateStreamingLocator(client, config.ResourceGroup, config.AccountName, outputAsset.Name, locatorName);
+
+                    IList<string> urls = GetStreamingURLs(client, config.ResourceGroup, config.AccountName, locator.Name);
+                    foreach (var url in urls)
+                    {
+                        Console.WriteLine(url);
+                    }
+                }
+<<<<<<< HEAD
+
+                Console.WriteLine("Done. Copy and paste the Streaming URL into the Azure Media Player at http://ampdemo.azureedge.net/");
+                Console.WriteLine("Press Enter to Continue");
+                Console.ReadLine();
+            }           
+=======
+            }
+            catch (ApiErrorException ex)
+            {
+                Console.WriteLine("{0}", ex.Message);
+
+                Console.WriteLine("Code: {0}", ex.Body.Error.Code);
+                Console.WriteLine("Message: {0}", ex.Body.Error.Message);
+            }
+>>>>>>> f60306445f13451672f80a514b740f6ccffb1ac7
+        }
+        
+        private static IAzureMediaServicesClient CreateMediaServicesClient(ConfigWrapper config)
+        {
+            ArmClientCredentials credentials = new ArmClientCredentials(config);
+
+            return new AzureMediaServicesClient(config.ArmEndpoint, credentials)
+            {
+                SubscriptionId = config.SubscriptionId,
+            };
+        }
+
+        private static Transform EnsureTransformExists(IAzureMediaServicesClient client,
+            string resourceGroupName,
+            string accountName, 
+            string transformName)
+        {
+            Transform transform = client.Transforms.Get(resourceGroupName, accountName, transformName);
+
+            if (transform == null)
+            {
+                var output = new[]
+                {
+                    new TransformOutput
+                    {
+                        // The preset for the Transform is set to one of our built-in sample presets.
+                        // You can  customize the encoding settings by changing this to use "StandardEncoderPreset" class.
+                        Preset = new BuiltInStandardEncoderPreset()
+                        {
+                            // This sample uses the built-in encoding preset for Adaptive Bitrate Streaming.
+                            PresetName = EncoderNamedPreset.AdaptiveStreaming
+                        }
+                    }
+                };
+
+                transform = new Transform(output);
+                transform = client.Transforms.CreateOrUpdate(resourceGroupName, accountName, transformName, output);
+            }
+
+            return transform;
+        }
+
+        private static Job SubmitJob(IAzureMediaServicesClient client,
+            string resourceGroup,
+            string accountName,
+            string transformName,
+            string outputAssetName,
+            string jobName)
+        {
+
+            // This is an example ingest from HTTPs source URL - a new feature of v3 API.  Change the URL to any accessible HTTPs URL or SAS URL from Azure. 
+            JobInputHttp jobInput =
+                new JobInputHttp(files: new[] { "https://nimbuscdn-nimbuspm.streaming.mediaservices.windows.net/2b533311-b215-4409-80af-529c3e853622/Ignite-short.mp4" });
+
+            JobOutput[] jobOutputs =
+            {
+                new JobOutputAsset(outputAssetName),
+            };
+
+            Job job = client.Jobs.Create(
+                resourceGroup, accountName,
+                transformName,
+                jobName,
+                new Job
+                {
+                    Input = jobInput,
+                    Outputs = jobOutputs,
+                });
+
+            return job;
+        }
+        private static Job WaitForJobToFinish(IAzureMediaServicesClient client,
+            string resourceGroupName,
+            string accountName,
+            string transformName,
+            string jobName)
+        {
+            int SleepInterval = 60 * 1000;
+
+            Job job = null;
+
+            while (true)
+            {
+                job = client.Jobs.Get(resourceGroupName, accountName, transformName, jobName);
+
+                if (job.State == JobState.Finished || job.State == JobState.Error || job.State == JobState.Canceled)
+                {
+                    break;
+                }
+
+                Console.WriteLine($"Job is {job.State}.");
+                for (int i = 0; i < job.Outputs.Count; i++)
+                {
+                    JobOutput output = job.Outputs[i];
+                    Console.Write($"\tJobOutput[{i}] is {output.State}.");
+                    if (output.State == JobState.Processing)
+                    {
+                        Console.Write($"  Progress: {output.Progress}");
+                    }
+                    Console.WriteLine();
+                }
+                System.Threading.Thread.Sleep(SleepInterval);
+            }
+
+            return job;
+        }
+
+        private static StreamingLocator CreateStreamingLocator(IAzureMediaServicesClient client,
+            string resourceGroup,
+            string accountName,
+            string assetName,
+            string locatorName)
+        {
+            StreamingLocator locator =
+                client.StreamingLocators.Create(resourceGroup,
+                accountName,
+                locatorName,
+                new StreamingLocator()
+                {
+                    AssetName = assetName,
+                    StreamingPolicyName = PredefinedClearStreamingOnly,
+                });
+
+            return locator;
+        }
+
+        static IList<string> GetStreamingURLs(
+            IAzureMediaServicesClient client,
+            string resourceGroupName,
+            string accountName,
+            String locatorName)
+        {
+            IList<string> streamingURLs = new List<string>();
+
+            string streamingUrlPrefx = "";
+
+            StreamingEndpoint streamingEndpoint = client.StreamingEndpoints.Get(resourceGroupName, accountName, "default");
+
+            if (streamingEndpoint != null)
+            {
+                streamingUrlPrefx = streamingEndpoint.HostName;
+                if(streamingEndpoint.ResourceState != StreamingEndpointResourceState.Running)
+                    client.StreamingEndpoints.Start(resourceGroupName, accountName, "default");
+            }
+
+
+            foreach (var path in client.StreamingLocators.ListPaths(resourceGroupName, accountName, locatorName).StreamingPaths)
+            {
+                streamingURLs.Add("http://" + streamingUrlPrefx + path.Paths[0].ToString());
+            }
+
+            return streamingURLs;
+        }
+
+        private static void DownloadResults(IAzureMediaServicesClient client,
+            string resourceGroup,
+            string accountName,
+            string assetName,
+            string resultsFolder)
+        {
+            
+            AssetContainerSas assetContainerSas = client.Assets.ListContainerSas(
+                                                                                resourceGroup, 
+                                                                                accountName, 
+                                                                                assetName,
+                                                                                permissions: AssetContainerPermission.Read,
+                                                                                expiryTime: DateTime.UtcNow.AddHours(1)
+                                                                                );
+
+            Uri containerSasUrl = new Uri(assetContainerSas.AssetContainerSasUrls.FirstOrDefault());
+            CloudBlobContainer container = new CloudBlobContainer(containerSasUrl);
+
+            string directory = Path.Combine(resultsFolder, assetName);
+            Directory.CreateDirectory(directory);
+
+            Console.WriteLine("Downloading results to {0}.", directory);
+
+            foreach (IListBlobItem blobItem in container.ListBlobs(null, true, BlobListingDetails.None))
+            {
+                if (blobItem is CloudBlockBlob)
+                {
+                    CloudBlockBlob blob = blobItem as CloudBlockBlob;
+                    string filename = Path.Combine(directory, blob.Name);
+
+                    blob.DownloadToFile(filename, FileMode.Create);
+                }
+            }
+
+            Console.WriteLine("Download complete.");
+        }
+
+        static void CleanUp(IAzureMediaServicesClient client,
+            string resourceGroupName,
+            string accountName, 
+            string transformName)
+        {
+            foreach (var job in client.Jobs.List(resourceGroupName, accountName, transformName))
+            {
+                client.Jobs.Delete(resourceGroupName, accountName, transformName, job.Name);
+            }
+
+            foreach (var asset in client.Assets.List(resourceGroupName, accountName))
+            {
+                client.Assets.Delete(resourceGroupName, accountName, asset.Name);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+# Azure Media Services v3 quickstart starter samples
+
+The projects in this repository support the Azure Media Services v3 quickstart topics:
+
+* EncodeAndStreamFiles.csproj - [Stream files](https://docs.microsoft.com/azure/media-services/latest/stream-files-dotnet-quickstart)
+
+## Prerequisites
+
+To run samples in this repository, you need:
+
+* If you do not have Visual Studio installed, you can get [Visual Studio Community 2017, Visual Studio Professional 2017, or Visual Studio Enterprise 2017](https://www.visualstudio.com/downloads/).
+* An Azure Media Services account. See the steps described in [Create a Media Services account](https://docs.microsoft.com/azure/media-services/latest/create-account-cli-quickstart).
+
+## To run each project in the solution
+
+* Select it as the **Set as Startup project**.
+* Add appropriate values to App.config.
